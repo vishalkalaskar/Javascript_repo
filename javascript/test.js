b5a2b2f4377b8a6d20ca3f3ebde7c388cebc6370
vishal kalaskar <83940132+vishalkalaskar@users.noreply.github.com>
2025-03-13 08:33:58 +0000

    second

--- javascript/test.js
+++ javascript/test.js
@@ -1,1 +1,15 @@
-console.log("hi ths js");
+console.log("hi this js");
+//const - once assign value not change.
+const accountId =1233434
+//let,var ,default variable can change.
+let accountEmail = "myemailId@gmail.com"
+var accountPassword = "123434"
+accountCity = "jaipur"
+let accountState;
+
+//accountId=12121212 //not allowed
+accountEmail="hc.hc@gmail.com"
+accountPassword="11111111"
+accountCity="mumbai"
+//Prefer not to use var becoz of issue in block scope and functional scope.
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
